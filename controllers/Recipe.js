0746fe05c167580fc91ce426638f5ef72a64e103
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-15 16:58:24 +0300

    ft:getting_recipe_by_id_controller:backend

--- controllers/Recipe.js
+++ controllers/Recipe.js
@@ -38,3 +38,28 @@ exports.postRecipe = async (req,res) => {
         })
     }
 }
+
+exports.getRecipeById = async(req,res) => {
+    try{
+        const id = req.params.id 
+        const recipe = await Recipe.findById(id)
+        if (recipe){
+            res.status(200).json({
+                success:true,
+                data:recipe
+            })
+        } else {
+            res.status(404).json({
+                success:false,
+                message:'Recipe not found'
+            })
+        }
+        
+    } catch (err){
+        console.log(`Error! :${err}`.red.bold)
+        return res.status(500).json({
+            success:false,
+            message:'Internal Server Error'
+        })
+    }
+}
