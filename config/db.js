d43dfcd984f1c20b2b37baa85a922df460f586c8
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-18 18:39:49 +0300

    ft:adding_user_register_test:backend

--- config/db.js
+++ config/db.js
@@ -1,17 +1,37 @@
 const mongoose = require('mongoose')
+const Mockgoose = require('mockgoose').Mockgoose
 const colors = require('colors')
 
 const connectDB = async () => {
-    try{ 
-        const conn = await mongoose.connect(process.env.MONGO_URI,{
-            useCreateIndex:true,
-            useUnifiedTopology:true,
-            useNewUrlParser:true
-        })
-        console.log(`MongoDb connected on ${conn.connection.host}`.cyan.bold)
-    } catch (err){
-        console.log(`err:${err}`)
-        process.exit(1)
+    if (process.env.NODE_ENV === 'test'){
+        // const mockgoose = new Mockgoose(mongoose)
+        // mockgoose.prepareStorage[]
+        //     .then()
+        try {
+            const mockgoose = new Mockgoose(mongoose)
+            const mocks = await mockgoose.prepareStorage()
+            const conn = await mongoose.connect(process.env.MONGO_URI,{
+                useCreateIndex:true,
+                useUnifiedTopology:true,
+                useNewUrlParser:true
+            })
+            console.log(`Mockgoose connected on ${conn.connection.host}`.cyan.bold)
+        } catch (err){
+            console.log(`err:${err}`)
+            process.exit(1)
+        }
+    } else {
+        try{ 
+            const conn = await mongoose.connect(process.env.MONGO_URI,{
+                useCreateIndex:true,
+                useUnifiedTopology:true,
+                useNewUrlParser:true
+            })
+            console.log(`MongoDb connected on ${conn.connection.host}`.cyan.bold)
+        } catch (err){
+            console.log(`err:${err}`)
+            process.exit(1)
+        }
     }
 }
 module.exports = connectDB
